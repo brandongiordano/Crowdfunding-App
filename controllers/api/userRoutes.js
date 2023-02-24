99d044f69826367c7370a9cdb84024ab2dc174d9
brandongiordano <bgiordano9917@aol.com>
2023-02-23 19:20:27 -0500

    fixed typo on homepage view causing no blogs to display

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -19,7 +19,7 @@ router.post('/', async (req, res) => {
 router.post('/login', async (req, res) => {
   try {
     const userData = await User.findOne({ 
-			where: { username: req.body.username } 
+			where: { name: req.body.name } 
 		});
 
     if (!userData) {
